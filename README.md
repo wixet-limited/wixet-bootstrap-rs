0914134df8e1efd2bcf66667514c07753c3f9b74
Alvaro Garcia <maxpowel@gmail.com>
2023-06-27 11:20:12 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,46 @@
+# wixet-bootstrap
+
+First of all, this library only makes sense if you use `tokio` and your application runs in the asynchronous way.
+
+Some code is used everytime. This library is the case for Wixet. If there is something common in all applications, this is
+a boot process. For simple programs or a "hello world" it is just simple but as long as your application grows you always
+implement it somehow.
+
+This library provides a simple but useful minimal boot process:
+* Configure logger (format and option to write a file) using [fern](https://github.com/daboross/fern)
+* Friendly exit and interrupt handler (ctr+c...) using [signal-hook](https://github.com/vorner/signal-hook)
+
+# How to use it
+
+Take this simple example
+```rust
+use wixet_bootstrap::init;
+use log::info;
+
+#[tokio::main]
+async fn main() {
+    info!("This log line will be ignored because the logger is not configured yet");
+    let (closer, exit) = init(Some("output.log")).await?; //If you provide None, it simple will not write a log file (just output)
+    info!("Hello to my application!")
+
+    // Do may awesome stuff spawing tokio tasks
+
+    // I use select here because it is common to listen for multiple signals, but you can just await the `exit` if not
+    tokio::select!{
+        _ = exit.recv_async() => {
+            info!("Shutdown process started");
+            // Do your friendly stop process here
+            // This code is run when ctrl+c or any other kill interrupt is received
+        }
+    };
+
+    // A friendly shutdown by deinitializing all "init" stuff.
+    closer.stop().await?;
+    info!("Bye");
+
+}
+```
+
+As you can see, it is very simple and easy to use but it saves many lines of code. And the most important thing, if we add a new feature/improvement it will apply to all the projects.
+
+I want to keep this library as simple and generic as possible but if you find something interesting to add I'll be glad to hear it!
---
+++ src/lib.rs
@@ -0,0 +1,115 @@
+#![warn(rustdoc::invalid_rust_codeblocks)]
+#![warn(missing_docs)]
+#![warn(rustdoc::missing_crate_level_docs)]
+
+//! This library just starts and configures a logger and provides a friendly shutdown process for tokio apps.
+//! Check [init] to see how to use it
+use signal_hook::consts::signal::*;
+use signal_hook_tokio::{Signals, Handle};
+use futures::stream::StreamExt;
+use log::info;
+use anyhow::Result;
+use tokio::task::JoinHandle;
+
+/// Signal handler. It publish a message into the shutdown channel
+async fn handle_signals(mut signals: Signals, tx: flume::Sender<i32>) {
+    while let Some(signal) = signals.next().await {
+        match signal {
+            SIGHUP => {
+                info!("Reloading");
+            }
+            SIGTERM | SIGINT | SIGQUIT => {
+                info!("Exit signal received, doing friendly shutdown...");
+                tx.send_async(0).await.unwrap();
+                // Shutdown the system;
+            },
+            _ => unreachable!(),
+        }
+    }
+}
+
+/// Configures the logger format. If outfile is none, no log file will be written
+pub fn setup_logger(outfile: Option<&str>) -> Result<()>{
+    let mut chain = fern::Dispatch::new()
+    // Perform allocation-free log formatting
+    .format(|out, message, record| {
+        out.finish(format_args!(
+            "{}[{}][{}] {}",
+            chrono::Local::now().format("[%Y-%m-%d][%H:%M:%S]"),
+            record.target(),
+            record.level(),
+            message
+        ))
+    })
+    // Add blanket level filter -
+    .level(log::LevelFilter::Info);
+    
+    if let Some(outfile) = outfile {
+        chain = chain.chain(fern::log_file(outfile)?);
+    }
+    chain.chain(std::io::stdout())
+    
+    .apply()?;
+    Ok(())
+
+}
+
+/// The shutdown context. It stores information about the signal handler task
+pub struct InitContext {
+    handle: Handle,
+    join: JoinHandle<()>
+}
+
+impl InitContext {
+    /// When the program ends, you should call this to friendly stop the signal handler
+    pub async fn stop(self) -> Result<()> {
+        self.handle.close();
+        self.join.await?;
+        Ok(())
+    }
+}
+
+/// The main function. Call it as soon as possible in your program. It returns the context
+/// and the channel to listen the exit requests
+/// 
+/// ```
+/// use wixet_bootstrap::init;
+/// use log::info;
+/// 
+/// #[tokio::main]
+/// async fn main() {
+///     info!("This log line will be ignored because the logger is not configured yet");
+///     let (closer, exit) = init(Some("output.log")).await?; //If you provide None, it simple will not write a log file (just output)
+///     info!("Hello to my application!")
+/// 
+///     // Do may awesome stuff spawing tokio tasks
+/// 
+///     // I use select here because it is common to listen for multiple signals, but you can just await the `exit` if not
+///     tokio::select!{
+///         _ = exit.recv_async() => {
+///         info!("Shutdown process started");
+///         // Do your friendly stop process here
+///         // This code is run when ctrl+c or any other kill interrupt is received
+///     }
+/// };
+/// 
+/// // A friendly shutdown by deinitializing all "init" stuff.
+/// closer.stop().await?;
+/// info!("Bye");
+/// 
+/// }
+/// ```
+pub async fn init(log_file: Option<&str>) -> Result<(InitContext, flume::Receiver<i32>)> {
+    setup_logger(log_file)?;
+    // Signals
+    let signals = Signals::new([
+        SIGHUP,
+        SIGTERM,
+        SIGINT,
+        SIGQUIT,
+    ])?;
+    let handle = signals.handle();
+    let (tx, rx) = flume::unbounded();
+    let signals_task = tokio::spawn(handle_signals(signals, tx));
+    Ok((InitContext{handle, join: signals_task}, rx))
+}
